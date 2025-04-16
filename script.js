3466f4f7fcc990e6ff7623603d182c6e1d28cd54
Vctr-777 <125025169+Victoredu1709@users.noreply.github.com>
2025-04-16 11:54:06 -0300

    rrg

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+const botaoDeAcessibilidade = document.getElementById('botao-Acessibilidade')
+const opçõesDeAcessibilidade = document.getElementById('opções-Acessibilidade')
+
+botaoDeAcessibilidade.addEventListtener('click', function(){
+botaoDeAcessibilidade.classList.toggle('rotação botão')
+opçõesDeAcessibilidade.classList.toggle('apressenta-lista')
+const botaoSelecionado =  botaoDeAcessibilidade
+}
